7ce3b80e1838d3a5a1b260817645cc9712573959
Yuki Tachi <z.yuki.tachi@gree.net>
2020-05-18 14:43:10 +0900

    Initial Commit

---
+++ BluetoothPeripheral/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BluetoothPeripheral
+//
+//  Created by Yuki Tachi on 2020/05/18.
+//  Copyright © 2020 Yuki Tachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
